1e9b5e268700c50dcfa057693ce0efdde2954ef5
anwam <dev.anuwong@gmail.com>
2024-12-05 15:14:16 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container flex items-center justify-center h-screen mx-auto">
+      <h1 className="font-serif text-bold">Boondariga & Anuwong</h1>
+    </div>
+  );
+}
+
+export default App;
